bff7522913c582e29ff632bcbc745520440c2498
ahka07001 <ahka07001@E-PW05CGQT>
2024-09-12 14:33:47 +0200

    Add project files.

---
+++ Inlämningsuppgift 1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Inlämningsuppgift1
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\");
+
+        }
+
+
+
+    }
+}
